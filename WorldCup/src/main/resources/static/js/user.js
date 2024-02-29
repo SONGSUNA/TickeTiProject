89b009a5b7886d13693c03dd5dd4ef5a030f0bda
BBangqu <bangjong21@naver.com>
2024-02-29 17:01:02 +0900

    user

--- WorldCup/src/main/resources/static/js/user.js
+++ WorldCup/src/main/resources/static/js/user.js
@@ -2,39 +2,52 @@ function createAccountForm() {
 	console.log('createAccountForm()');
 	
 	let form = document.create_account_form;
+	
 	if (form.u_id.value === '') {
-		alert('아이디 입력칸이 비었습니다. 아이디를 입력해주세요');
+		alert('아이디를 입력해주세요.');
 		form.u_id.focus();
 		
 	} else if (form.u_pw.value === '') {
-		alert('비밀번호 입력칸이 비었습니다. 비밀번호를 입력해주세요');
+		alert('비밀번호를 입력해주세요.');
 		form.u_pw.focus();
 		
-	} else if (form.u_mail.value === '') {
-		alert('이메일 입력칸이 비었습니다. 이메일을 입력해주세요');
+	} else if (form.u_pw.value !== form.u_pw_confirm.value) {
+		alert('등록한 비밀번호가 일치하지않습니다.');
+		form.u_pw.focus();
+		
+	} else if (form.u_name.value === '') {
+		alert('이름을 입력해주세요.');
 		form.u_mail.focus();
 		
-	} else if (form.u_phone.value === '') {
-		alert('연락처 입력칸이 비었습니다. 연락처를 입력해주세요');
-		form.u_phone.focus();
+	} else if (form.u_reg_num_pre.value === '') {
+		alert('주민번호를 입력해주세요.');
+		form.u_reg_num_pre.focus();
+		
+	}else if (form.u_reg_num_suf.value === '') {
+		alert('주민번호를 입력해주세요.');
+		form.u_reg_num_suf.focus();
+		
+	} else if (form.u_mail_pre.value === '') {
+		alert('이메일을 입력해주세요.');
+		form.u_mail_pre.focus();
+		
+	} else if (form.u_mail_suf.value === '') {
+		alert('이메일을 입력해주세요.');
+		form.u_mail_suf.focus();
 		
 	} else if (form.postcode.value === '') {
-		alert('우편번호가 비었습니다.');
+		alert('우편번호를 입력해주세요.');
 		form.postcode.focus();
 		
 	} else if (form.address.value === '') {
-		alert('주소가 비었습니다.');
-		form.postcode.focus();
-		
-	} else if (form.detailAddress.value === '') {
-		alert('INPUT PHONE!!');
-		form.postcode.focus();
+		alert('주소를 입력해주세요');
+		form.address.focus();
 		
 	}else if (form.extraAddress.value === '') {
-		alert('INPUT PHONE!!');
-		form.postcode.focus();
+		alert('주소 참고항목을 입력해주세요.');
+		form.extraAddress.focus();
 		
-	}else {
+	} else {
 		form.submit();
 		
 	}
