0d4e19fd747f4bcdc7a45fcd0a090cc57e72c050
BBangqu <bangjong21@naver.com>
2024-03-04 10:45:17 +0900

    productPAGE

---
+++ TicketReserve/src/main/java/com/office/worldcup/productpage/ProductController.java
@@ -0,0 +1,22 @@
+package com.office.worldcup.productpage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/product_page")
+@Log4j2
+public class ProductController {
+	
+	@GetMapping("")
+	public String productPage() {
+		log.info("productPage()");
+		
+		String nextPage = "/product_page/product_page";
+		
+		return nextPage;
+	}
+}
