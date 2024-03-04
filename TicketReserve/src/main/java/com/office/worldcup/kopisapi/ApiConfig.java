d2bdfc8dc5cc8f048e9861f016b502201fd12ab2
ysw <tkddnjs10@naver.com>
2024-03-04 18:55:57 +0900

    api

---
+++ TicketReserve/src/main/java/com/office/worldcup/kopisapi/ApiConfig.java
@@ -0,0 +1,11 @@
+package com.office.worldcup.kopisapi;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiConfig {
+	
+	final static public String API_URL = "https://www.kopis.or.kr/openApi/restful/";
+	final static public String API_KEY = "2463fcf1933f401ba189b394095fa3fa";
+
+}
