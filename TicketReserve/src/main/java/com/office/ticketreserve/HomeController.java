ba63805a611b9f2890685b2e1e718043919b1bf2
ysw <tkddnjs10@naver.com>
2024-03-05 10:19:09 +0900

    change package name

---
+++ TicketReserve/src/main/java/com/office/ticketreserve/HomeController.java
@@ -0,0 +1,35 @@
+package com.office.ticketreserve;
+
+import javax.xml.bind.JAXBException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.office.ticketreserve.kopisapi.ApiConfig;
+import com.office.ticketreserve.kopisapi.KopisApi;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Controller
+@RequestMapping("")
+public class HomeController  {
+	
+	@Autowired
+	ApiConfig apiConfig;
+
+	@GetMapping({"/", ""})
+	public String home() throws JAXBException {
+		log.info("[HomeController] home()");
+		
+		String nextPage = "/home";
+		
+		KopisApi api = new KopisApi();
+		api.getMusicalInfo();
+		
+		return nextPage;
+	}
+	
+}
---
+++ TicketReserve/src/main/java/com/office/ticketreserve/kopisapi/ApiConfig.java
@@ -0,0 +1,10 @@
+package com.office.ticketreserve.kopisapi;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiConfig {
+	
+	final static public String API_URL = "https://www.kopis.or.kr/openApi/restful/";
+	final static public String API_KEY = "2463fcf1933f401ba189b394095fa3fa";
+}
