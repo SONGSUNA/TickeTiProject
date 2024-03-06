e84307ddc1a8e8c0f9ace3717fc8a55a1d78e06c
BBangqu <bangjong21@naver.com>
2024-03-06 14:27:26 +0900

    home

---
+++ TicketReserve/src/main/resources/static/js/review/review_result.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var reviewExitBtn = document.querySelector('.reviewExitBtn');
+	reviewExitBtn.addEventListener('click', function() {
+       window.close();
+    });
+    });
