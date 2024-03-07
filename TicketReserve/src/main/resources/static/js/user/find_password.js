963f963cf9f938db40b65770484cb8fccc669dd8
SUNA <snsong91@gmail.com>
2024-03-08 02:45:23 +0900

    이메 의존성 변경 및 아이디찾기 폼

--- TicketReserve/src/main/resources/static/js/user/find_password.js
+++ TicketReserve/src/main/resources/static/js/user/find_password.js
@@ -23,3 +23,29 @@ function findPassword() {
 	}
 	 
 }
+
+function findIdBtn() {
+	console.log("findIdBtn() clicked!!");
+	
+	let form = document.user_password_find;
+	let u_mail = form.u_mail_pre.value + "@" + form.u_mail_suf.value;
+
+	form.u_mail.value = u_mail;
+	
+	if(form.u_name.value == '') {
+		 alert('아이디를 입력해주세요.');
+    	 form.u_id.focus();
+    	 
+	} else if(form.u_mail_pre.value == ''){
+		 alert('메일을 입력해주세요.');
+    	 form.u_mail_pre.focus();
+    	 
+	} else if(form.u_mail_suf.value == ''){
+		 alert('메일주소를 확인해주세요.');
+    	 form.u_mail_suf.focus();
+    	 
+	} else {
+		form.submit();
+	}
+	 
+}
