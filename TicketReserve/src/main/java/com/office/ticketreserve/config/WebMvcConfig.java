9b3cef075bc67ee464b2ad931685edf97708f647
bombombobm <studyjh951002@gmail.com>
2024-03-07 16:19:18 +0900

    ing

--- TicketReserve/src/main/java/com/office/ticketreserve/config/WebMvcConfig.java
+++ TicketReserve/src/main/java/com/office/ticketreserve/config/WebMvcConfig.java
@@ -33,7 +33,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	    		.excludePathPatterns("/user/create_account_confirm")
 	    		.excludePathPatterns("/user/checkId")
 	    		.excludePathPatterns("/user/user_login_form")
-	    		.excludePathPatterns("/user/user_login_confirm")
 	    		.excludePathPatterns("/user/user_login_confirm");
 	}
 	
