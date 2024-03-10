74167d4d2394692d5e70253dcc2b3842c77ecccd
ysw <tkddnjs10@naver.com>
2024-03-10 15:25:51 +0900

    home Start

--- TicketReserve/src/main/java/com/office/ticketreserve/HomeController.java
+++ TicketReserve/src/main/java/com/office/ticketreserve/HomeController.java
@@ -1,14 +1,18 @@
 package com.office.ticketreserve;
 
+import java.util.List;
+
 import javax.xml.bind.JAXBException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.office.ticketreserve.kopisapi.KopisApi;
 import com.office.ticketreserve.kopisapi.TicketDB;
+import com.office.ticketreserve.productpage.CurrentReserveDto;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -24,15 +28,15 @@ public class HomeController  {
 	HomeService homeService;
 	
 	@GetMapping({"/", ""})
-	public String home() throws JAXBException {
+	public String home(Model model) throws JAXBException {
 		log.info("[HomeController] home()");
 		
 		String nextPage = "/home";
 		api.getMusicalInfo(3);		//인자값 1주면 전체 API땡겨옴 2주면 예매현황 땡겨옴 3주면 그냥 넘어감.
 		//tDB.TicketStringFix();
 		
-		//List<TicketDto> rankOnePerfos = homeService.getRankOnePerfo();
-		
+//		List<CurrentReserveDto> rankOnePerfos = homeService.getRankOnePerfo();
+//		model.addAttribute("rankOnePerfos", rankOnePerfos);
 		
 		return nextPage;
 	}
