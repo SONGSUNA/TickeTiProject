8f3bfbcd445aff398ed77a804cb477fe1c0eca06
BBangqu <bangjong21@naver.com>
2024-03-15 09:30:49 +0900

    like

--- TicketReserve/src/main/java/com/office/ticketreserve/productpage/ProductController.java
+++ TicketReserve/src/main/java/com/office/ticketreserve/productpage/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.office.ticketreserve.review.ReviewDto;
@@ -27,25 +29,51 @@ public class ProductController {
 	ReviewService reviewService;
 	
 	
+//	@RequestMapping("/product/{p_id}")
+//	public String productPage(@PathVariable("p_id") String p_id, Model model) {
+//		log.info("productPage()");
+//		
+//		
+//		PerfomanceDto productDto = productService.productPage(p_id);
+//
+//		List<ReviewDto> reviewDtos = reviewService.allReviewsForPid(p_id);
+//		
+//		model.addAttribute("productDto", productDto);
+//		model.addAttribute("reviewDtos", reviewDtos);
+//		
+//		
+//		
+//		String nextPage = "/product_page/product_page";
+//		
+//		return nextPage;
+//	}
 	@RequestMapping("/product/{p_id}")
-	public String productPage(@PathVariable("p_id") String p_id, Model model) {
-		log.info("productPage()");
-		
-		
-		PerfomanceDto productDto = productService.productPage(p_id);
+	public String productPage(@PathVariable("p_id") String p_id, Model model, HttpSession session) {
+	    log.info("productPage()");
+	    PerfomanceDto productDto = productService.productPage(p_id);
+	    List<ReviewDto> reviewDtos = reviewService.allReviewsForPid(p_id);
 
-		List<ReviewDto> reviewDtos = reviewService.allReviewsForPid(p_id);
-		
-		model.addAttribute("productDto", productDto);
-		model.addAttribute("reviewDtos", reviewDtos);
-		
-		
-		
-		String nextPage = "/product_page/product_page";
-		
-		return nextPage;
+	    UserDto loginedUserDto = (UserDto) session.getAttribute("loginedUserDto");
+	    boolean isLiked = false;
+	    if (loginedUserDto != null) {
+	        isLiked = productService.isLiked(p_id, loginedUserDto.getU_id());
+	    }
+
+	    model.addAttribute("productDto", productDto);
+	    model.addAttribute("reviewDtos", reviewDtos);
+	    model.addAttribute("isLiked", isLiked);
+	    model.addAttribute("isLogined", loginedUserDto != null);
+
+	    String nextPage = "/product_page/product_page_test";
+	    return nextPage;
+	}
+	@ResponseBody
+	@PostMapping("/product/like")
+	public int productLike(@RequestParam("p_id") String p_id,
+	                       @RequestParam("u_id") String u_id) {
+	    log.info("좋아요 토글");
+	    return productService.toggleLike(p_id, u_id);
 	}
 	
 	
-
 }
--- TicketReserve/src/main/java/com/office/ticketreserve/productpage/ProductService.java
+++ TicketReserve/src/main/java/com/office/ticketreserve/productpage/ProductService.java
@@ -13,6 +13,21 @@ public class ProductService {
 		
 		return productDao.selectProduct(pId);
 	}
-
-
+	public int productLike(String p_id, String u_id) {
+		
+		return productDao.insertLike(p_id,u_id);
+	}
+	
+	public boolean isLiked(String p_id, String u_id) {
+	    return productDao.selectIsLiked(p_id, u_id);
+	}
+	
+	public int toggleLike(String p_id, String u_id) {
+	    boolean isLiked = productDao.selectIsLiked(p_id, u_id);
+	    if (isLiked) {
+	        return productDao.deleteLike(p_id, u_id);
+	    } else {
+	        return productDao.insertLike(p_id, u_id);
+	    }
+	}
 }
